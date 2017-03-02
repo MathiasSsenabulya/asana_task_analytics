49c6b01293c9a382a3b5b8b94f5bbf76c2ddadc7
Matvey Ustinov <matlex@yandex.ru>
2017-03-03 00:21:55 +0600

    Reimplement MIT and MIW handlers.

--- asana_task_analytics_app/views.py
+++ asana_task_analytics_app/views.py
@@ -24,7 +24,7 @@ class BaseMitAndMiW:
         {'id': 28211362476024, 'name': 'Next Important This Week'}
         """
         # print(client.tags.find_all({'workspace': "1968482998660"}))
-        tasks = client.tasks.find_all({'tag': tag_id}, item_limit=item_limit)
+        tasks = client.tasks.find_all({'tag': tag_id, 'completed_since': 'now'}, item_limit=item_limit)
         return tasks
 
     def get_task_history_by_task_id(self, task_id):
@@ -33,21 +33,6 @@ class BaseMitAndMiW:
 
 
 class AsanaMitHandler(BaseMitAndMiW):
-    def check_completness_and_mit_from_task_stories(self, task_stories_list_dicts):
-        added_to_mit = False
-        completed_this_task = False
-        for history_record in task_stories_list_dicts:
-            if history_record['text'] == 'added to Most Important Today':
-                added_to_mit = True
-            elif history_record['text'] == 'removed from Most Important Today':
-                added_to_mit = False
-            elif history_record['text'] == 'completed this task':
-                completed_this_task = True
-        if added_to_mit and not completed_this_task:
-            return False
-        elif added_to_mit and completed_this_task:
-            return True
-
     def get_date_task_added_to_mit_from_task_story(self, task_stories_list_dicts):
         output = {}
         added_to_mit = None
@@ -72,37 +57,20 @@ class AsanaMitHandler(BaseMitAndMiW):
         tasks = self.get_tasks_by_tag_id(settings.ASANA_TAG_ID, self.item_limit)
         for task in tasks:
             stories = list(self.get_task_history_by_task_id(task['id']))
-            task_completed = self.check_completness_and_mit_from_task_stories(stories)
-            if not task_completed:
-                task_info_output = {'task_id': task['id'], 'task_title': task['name']}
-                date_added_to_mit = self.get_date_task_added_to_mit_from_task_story(stories)
-                task_info_output.update(date_added_to_mit)
-                top_latent_mit.append(task_info_output)
+            task_info_output = {'task_id': task['id'], 'task_title': task['name']}
+            date_added_to_mit = self.get_date_task_added_to_mit_from_task_story(stories)
+            task_info_output.update(date_added_to_mit)
+            top_latent_mit.append(task_info_output)
 
-                # print(task)
-                # print(stories)
-                # print("")
+            # print(task)
+            # print(stories)
+            # print("")
 
         rows_by_added_to_mit = sorted(top_latent_mit, key=itemgetter('added_to_mit'))
         return rows_by_added_to_mit
 
 
 class AsanaMiwHandler(BaseMitAndMiW):
-    def check_completness_and_miw_from_task_stories(self, task_stories_list_dicts):
-        added_to_miw = False
-        completed_this_task = False
-        for history_record in task_stories_list_dicts:
-            if history_record['text'] == 'added to Most Important This Week':
-                added_to_miw = True
-            elif history_record['text'] == 'removed from Most Important This Week':
-                added_to_miw = False
-            elif history_record['text'] == 'completed this task':
-                completed_this_task = True
-        if added_to_miw and not completed_this_task:
-            return False
-        elif added_to_miw and completed_this_task:
-            return True
-
     def get_date_task_added_to_miw_from_task_story(self, task_stories_list_dicts):
         output = {}
         added_to_miw = None
@@ -127,16 +95,14 @@ class AsanaMiwHandler(BaseMitAndMiW):
         tasks = self.get_tasks_by_tag_id(settings.ASANA_TAG_ID, self.item_limit)
         for task in tasks:
             stories = list(self.get_task_history_by_task_id(task['id']))
-            task_completed = self.check_completness_and_miw_from_task_stories(stories)
-            if not task_completed:
-                task_info_output = {'task_id': task['id'], 'task_title': task['name']}
-                date_added_to_miw = self.get_date_task_added_to_miw_from_task_story(stories)
-                task_info_output.update(date_added_to_miw)
-                top_latent_miw.append(task_info_output)
+            task_info_output = {'task_id': task['id'], 'task_title': task['name']}
+            date_added_to_miw = self.get_date_task_added_to_miw_from_task_story(stories)
+            task_info_output.update(date_added_to_miw)
+            top_latent_miw.append(task_info_output)
 
-                # print(task)
-                # print(stories)
-                # print("")
+            # print(task)
+            # print(stories)
+            # print("")
 
         rows_by_added_to_miw = sorted(top_latent_miw, key=itemgetter('added_to_miw'))
         return rows_by_added_to_miw
@@ -144,7 +110,7 @@ class AsanaMiwHandler(BaseMitAndMiW):
 
 # /api/scalar_metrics/mit/
 def top_latent_mit_tasks(request):
-    item_limit = 20
+    item_limit = None
     mit_handler = AsanaMitHandler(item_limit)
     output = mit_handler.get_top_latent_mit()
     return JsonResponse(output, safe=False)
@@ -152,7 +118,7 @@ def top_latent_mit_tasks(request):
 
 # /api/scalar_metrics/miw/
 def top_latent_miw_tasks(request):
-    item_limit = 20
+    item_limit = None
     miw_handler = AsanaMiwHandler(item_limit)
     output = miw_handler.get_top_latent_miw()
     return JsonResponse(output, safe=False)
